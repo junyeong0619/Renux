78e744110ec760e0401299dde12ff7219de4a2fb
Jun Yeong Kim <junyeonggim5@gmail.com>
2025-09-18 14:19:59 +0900

    Server accept more clients at the same time in MacOS
    By using kqueue, now server can accept more clients
    in MacOS. For this function new struct client_state
    added.
    
    Authored by Jun Yeong Kim

--- server/server.c
+++ server/server.c
@@ -14,11 +14,24 @@
 #include "service.h"
 #include "../utils/ssl_utils.h"
 #include "../utils/log.h"
+#ifdef __APPLE__
+    #include <sys/event.h>
+#elif __linux__
+    #include <sys/epoll.h>
+#endif
+
 
 #define PORT 8080
 #define BUF_SIZE 1024
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
+#define MAX_EVENTS 128
+
+
+typedef struct {
+    int fd;
+    int is_logged_in; // 0:logout 1:login
+} client_state;
 
 static void generate_socket(int *server_fd) {
     if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -106,6 +119,7 @@ int main() {
     }
     display_server_log("Server Listening succeeded Waiting for client connection...");
 
+
     safe_log_message_concat(username, "\tnow opened server.",log_message);
 
     //hashing server password
@@ -120,43 +134,110 @@ int main() {
     snprintf(hashed_msg, sizeof(hashed_msg), "Password has been hashed. Hash: %lu", server_passwd_hashed);
     display_server_log(hashed_msg);
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        display_server_log("Error: Accepting failed");
-        cleanup_server_tui();
+    int kq = kqueue();
+    if (kq == -1) {
+        perror("kqueue");
         exit(EXIT_FAILURE);
     }
-    display_server_log("Server accepting client...");
 
-    display_server_log("Connection established.");
+    struct kevent change_event;
+    struct kevent event_list[MAX_EVENTS];
 
-    const char *login_msg = "Put the login information in this form: username,password";
-    send(new_socket, login_msg, strlen(login_msg), 0);
+    client_state clients[MAX_EVENTS];
+    for (int i = 0; i < MAX_EVENTS; i++) {
+        clients[i].fd = -1;
+        clients[i].is_logged_in = 0;
+    }
 
-    display_server_log("Server waiting for client's input...");
-    valread = read(new_socket, buffer, BUF_SIZE - 1);
-    if (likely(valread > 0)) {
-        buffer[valread] = '\0';
+    EV_SET(&change_event, server_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
+    if (kevent(kq, &change_event, 1, NULL, 0, NULL) == -1) {
+        perror("kevent register");
+        exit(EXIT_FAILURE);
+    }
 
-        char data[BUF_SIZE];
-        strcpy(data, buffer);
+    display_server_log("Server is running with event loop...");
 
-        safe_log_message_concat(data, "Server received a response from client.\nclient's original response:\t%s", log_message);
+    while (1) {
+        int num_events = kevent(kq, NULL, 0, event_list, MAX_EVENTS, NULL);
+        if (num_events == -1) {
+            perror("kevent wait");
+            exit(EXIT_FAILURE);
+        }
 
-        char *get_user_name = strtok(data, ",");
-        char *password = strtok(NULL, ",\n");
-        safe_log_message_concat(get_user_name,"Server get username from response:\t%s",log_message);
-        safe_log_message_concat(password,"Server get password from response:\t%d",log_message);
+        for (int i = 0; i < num_events; i++) {
+            int event_fd = event_list[i].ident;
 
-        if (username != NULL && password != NULL) {
-            login_process(get_user_name, password, server_passwd_hashed, new_socket);
-            display_server_log("Server login succeeded.");
-        } else {
-            const char *failure_msg = "Login failed. incorrect form error";
-            send(new_socket, failure_msg, strlen(failure_msg), 0);
-            display_server_log("login Failed");
+            if (event_list[i].flags & EV_EOF) {
+                display_server_log("Client disconnected.");
+                close(event_fd);
+                for (int j = 0; j < MAX_EVENTS; j++) {
+                    if (clients[j].fd == event_fd) {
+                        clients[j].fd = -1;
+                        clients[j].is_logged_in = 0;
+                        break;
+                    }
+                }
+            } else if (event_fd == server_fd) {
+                int client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+                if (client_socket == -1) {
+                    perror("accept");
+                    continue;
+                }
+
+                EV_SET(&change_event, client_socket, EVFILT_READ, EV_ADD, 0, 0, 0);
+                kevent(kq, &change_event, 1, NULL, 0, NULL);
+
+                for (int j = 0; j < MAX_EVENTS; j++) {
+                    if (clients[j].fd == -1) {
+                        clients[j].fd = client_socket;
+                        break;
+                    }
+                }
+
+                display_server_log("New client connected.");
+                const char *login_msg = "Put the login information in this form: username,password";
+                send(client_socket, login_msg, strlen(login_msg), 0);
+
+            } else {
+                char buffer[BUF_SIZE] = {0};
+                ssize_t valread = read(event_fd, buffer, BUF_SIZE - 1);
+
+                if (valread > 0) {
+                    buffer[valread] = '\0';
+                    buffer[strcspn(buffer, "\n")] = 0;
+
+                    client_state* current_client = NULL;
+                    for (int j = 0; j < MAX_EVENTS; j++) {
+                        if (clients[j].fd == event_fd) {
+                            current_client = &clients[j];
+                            break;
+                        }
+                    }
+
+                    if (current_client == NULL) continue; // safety check
+
+                    if (current_client->is_logged_in) {
+                        handle_client_request(event_fd, buffer);
+                    } else {
+                        char data[BUF_SIZE];
+                        strcpy(data, buffer);
+                        char *get_user_name = strtok(data, ",");
+                        char *password = strtok(NULL, ",");
+
+                        if (get_user_name && password && is_valid_login(get_user_name, password, server_passwd_hashed) == 0) {
+                            current_client->is_logged_in = 1;
+                            send(event_fd, "logsuc", 6, 0);
+                            display_server_log("Client login successful.");
+                        } else {
+                            send(event_fd, "Login failed", 12, 0);
+                            display_server_log("Client login failed.");
+                        }
+                    }
+                }
+            }
         }
     }
-    
+
     server_cleanup(new_socket, server_fd, username);
 
     return 0;
